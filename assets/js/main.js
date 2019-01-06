6660fe56bb4985e4d3734c432af91368da0bf35c
Austin_Lin <au222cc@gmail.com>
2019-01-06 17:23:44 +0800

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,6 @@
 //onload要執行的程式
 window.onload=function() {
-    //getData('HTML5_video.html');
-    //realTime();
-    //videoPlay();
+    //realTime();   
 }
 
 
@@ -28,44 +26,3 @@ function realTime() {
 }
 
 
-
-//ajax部份更新網頁
-function getData(element) {
-    var ajax1 = new XMLHttpRequest();
-    ajax1.open("get", "http://127.0.0.1:8887/" + element);
-    ajax1.onload = function () {
-        var XXX = document.getElementById("ajaxChange");
-        XXX.innerHTML = this.responseText;
-    }
-    ajax1.send();
-}
-
-/*
-//影片播放
-var video;
-function videoPlay () {
-    video = document.getElementById("video");
-    video.addEventListener("timeupdate", updateTimer);
-    video.addEventListener("ended", function () {
-        alert("Ended");
-    });
-}
-
-function updateTimer() {
-    var timer = document.getElementById("timer");
-    timer.innerHTML = video.currentTime + "/" + video.duration;
-}
-
-function playVideo() {
-    video.play();
-}
-
-function pauseVideo() {
-    video.pause();
-}
-
-function replayVideo() {
-    video.currentTime = 0;
-    video.play();
-}
-*/
