eab3e45e8ddae59aaa74193e72e283aefd9e259c
Austin_Lin <au222cc@gmail.com>
2019-01-12 19:35:39 +0800

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,6 @@
 //-----------------------onload要執行的程式
 window.onload = function () {
     //realTime();   
-    //autoShowSlides();
 }
 
 
@@ -27,58 +26,7 @@ function realTime() {
 }
 
 
-//-----------------------------slideshow
-var slideIndex = 1;
-showSlides(slideIndex);
 
-function plusSlides(n) {
-    showSlides(slideIndex += n);
-}
-
-function currentSlide(n) {
-    showSlides(slideIndex = n);
-}
-var slideIndex = 0;
-
-function showSlides(n) {
-    var i;
-    var slides = document.getElementsByClassName("mySlides");
-    var dots = document.getElementsByClassName("dot");
-    if (n > slides.length) {
-        slideIndex = 1
-    }
-    if (n < 1) {
-        slideIndex = slides.length
-    }
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideIndex - 1].style.display = "block";
-    dots[slideIndex - 1].className += " active";
-}
-
-function autoShowSlides() {
-    var i;
-    var slides = document.getElementsByClassName("mySlides");
-    var dots = document.getElementsByClassName("dot");
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slideIndex++;
-    if (slideIndex > slides.length) {
-        slideIndex = 1
-    }
-    slides[slideIndex - 1].style.display = "block";
-    dots[slideIndex - 1].className += " active";
-    setTimeout(autoShowSlides, 4000);
-}
 //-----------------------RWD
 function myFunction() {
     var x = document.getElementById("myTopNav");
@@ -87,21 +35,4 @@ function myFunction() {
     } else {
         x.className = "menu";
     }
-}
-//-------------------googleMap
-
-function initMap() {
-    var myHomeLocation = {
-        lat: 25.06289,
-        lng: 121.50208
-    };
-    var map = new google.maps.Map(
-        document.getElementById('googleMap'), {
-            zoom: 4,
-            center: myHomeLocation
-        });
-    var marker = new google.maps.Marker({
-        position: myHomeLocation,
-        map: map
-    });
 }
